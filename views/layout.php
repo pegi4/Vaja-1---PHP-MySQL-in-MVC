699f66088c0a051fb89b4a035b7e25d76a06d44f
Vito Senic <67893442+pegi4@users.noreply.github.com>
2025-03-26 09:51:30 +0100

    layout changed for displaying my articles

--- views/layout.php
+++ views/layout.php
@@ -20,6 +20,7 @@
             if(isset($_SESSION["USER_ID"])){
                 ?>
                 <li class="nav-item"><a href="/articles/create" class="nav-link">Objavi novico</a></li>
+                <li class="nav-item"><a href="/articles/list" class="nav-link">Moje novice</a></li>
                 <li class="nav-item"><a href="/users/edit" class="nav-link">Uredi profil</a></li>
                 <li class="nav-item"><a href="/auth/logout" class="nav-link">Odjava</a></li>
                 <?php
@@ -46,6 +47,7 @@
                 if(isset($_SESSION["USER_ID"])){
                     ?>
                     <li class="nav-item"><a href="/articles/create" class="nav-link px-2 text-body-secondary">Objavi novico</a></li>
+                    <li class="nav-item"><a href="/articles/list" class="nav-link px-2 text-body-secondary">Moje novice</a></li>
                     <li class="nav-item"><a href="/users/edit" class="nav-link px-2 text-body-secondary">Uredi profil</a></li>
                     <li class="nav-item"><a href="/auth/logout" class="nav-link px-2 text-body-secondary">Odjava</a></li>
                     <?php
